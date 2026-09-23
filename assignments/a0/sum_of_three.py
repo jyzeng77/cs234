7e2e57f8e51ac4058750231b746ccdf486a59380
Jerry Zeng <77946782+jyzeng77@users.noreply.github.com>
2026-09-23 14:28:36 -0400

    Finish A0

---
+++ assignments/a0/sum_of_three.py
@@ -0,0 +1,18 @@
+##
+## =======================================================
+## Jerry Zeng (21188083)
+## CS 234 Fall 2026
+## Assignment 0, P1
+## =======================================================
+##
+
+def sum_of_three(num):
+    """
+    sum_of_three(num) takes sums an integer and sums it with numbers 
+        found on the Consulting_OfficeHours channel on Microsoft Teams
+        and the pinned FAQ for A0 on edX.
+    sum_of_three: Int -> Int
+    """
+    TEAMS_NUM = 45
+    EDX_NUM = 24
+    return TEAMS_NUM + EDX_NUM + num
